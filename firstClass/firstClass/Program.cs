f145e7a66743b22e0f0237130aa12a14270ee26b
Collin Guillaume <220053@CCM-Asty-Moulin.local>
2023-09-12 14:10:22 +0200

    12/09/2023

--- firstClass/firstClass/Program.cs
+++ firstClass/firstClass/Program.cs
@@ -4,13 +4,27 @@ using firstClass;
 string affiche;
 Console.WriteLine("Hello, World!");
 
-chien chien1 = new chien("plue","cockere",10);
-chien chien2 = new chien("toby", "buldog", 5);
-chien chien3 = new chien("seth", "berger allemen", 4);
+chien chien1 = new chien("plue","cockere",10, true,false,true,false,true);
+chien chien2 = new chien("toby", "buldog", 5, false,true,false,true,false);
+chien chien3 = new chien("seth", "berger allemend", 4, true,true,true,true,false);
 
 Console.WriteLine(chien1.afficheCaracteristique());
 Console.WriteLine(chien2.afficheCaracteristique());
 Console.WriteLine(chien3.afficheCaracteristique());
 
-
+Console.WriteLine(chien1.Age);
+chien1.Age =uint.Parse(Console.ReadLine());
+Console.WriteLine();
+string gen;
+if (chien1.Vaccin) { gen = "male"; } else { gen = "femelle"; }
+chien1.Vaccin = bool.Parse(Console.ReadLine());
+Console.WriteLine(chien1.Genre);
+chien1.Genre = bool.Parse(Console.ReadLine());
+Console.WriteLine(chien1.Sterelise);
+chien1.Sterelise = bool.Parse(Console.ReadLine());
+Console.WriteLine(chien1.Dresse);
+chien1.Dresse = bool.Parse(Console.ReadLine());
+Console.WriteLine(chien1.Toilette);
+chien1.Toilette = bool.Parse(Console.ReadLine());
+Console.WriteLine(chien1.afficheCaracteristique());
 
--- firstClass/firstClass/chien.cs
+++ firstClass/firstClass/chien.cs
@@ -17,16 +17,70 @@ namespace firstClass
         private bool _dresse;
         private bool _toilette;
 
-         public  chien(string nom, string race,uint age,bool )
+         public chien(string nom, string race, uint age, bool vaccin , bool genre , bool strerelise, bool dresse, bool toilette)
         {
             _nom = nom;
             _race = race;
             _age = age;
+            _vacccin = vaccin;
+            _genre = genre;
+            _strerelise=strerelise;
+            _dresse=dresse;
+            _toilette=toilette;
+
         }
+
+        public uint Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public bool Vaccin
+        {
+            get { return _vacccin; }
+            set { _vacccin = value; }
+        }
+
+        public bool Genre
+        {
+            
+            get { return _genre; }
+            set { _genre = value; }
+        }
+        public bool Sterelise
+        {
+            get { return _strerelise; }
+            set { _strerelise = value; }
+        }
+        public bool Dresse
+        {
+            get { return _dresse; }
+            set { _dresse = value; }
+        }
+        public bool Toilette
+        {
+            get { return _toilette; }
+            set { _toilette = value; }
+        }
+
+
 
         public string afficheCaracteristique ()
         {
-            string chaine = "nom : " + _nom + " - Age: " + _age + " -race: " +_race;
+            string Vacc;
+            if (_vacccin){ Vacc = "vacciné";}else{Vacc = "non vacciné";}
+            string gen;
+            if (_genre){ gen = "male"; }else{gen = "femelle";}
+            string ster;
+            if (_strerelise){ ster = "sterelisé"; }else{ ster = "non sterelisé"; }
+            string dress;
+            if (_dresse) { dress = "dresse"; } else { dress = "non dresse"; }
+            string toil;
+            if (_toilette) { toil = "toiletté"; } else { toil = "non toiletté"; }
+
+            string chaine = "nom : " + _nom + " - Age: " + _age + " -race: " +_race +" -vaccin: " + Vacc +" -sexe " +gen +"-sterilisation " 
+                +ster +" -dressage " + dress + " -toilletage "+toil ;
             return chaine;
         }
     }
