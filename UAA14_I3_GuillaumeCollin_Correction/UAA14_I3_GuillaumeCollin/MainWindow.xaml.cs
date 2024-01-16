0f64666f28506997aa180ecf4f92857361d9c4d3
guillaumecollin <220053@site.asty-moulin.be>
2024-01-16 13:21:55 +0100

    P-body

---
+++ UAA14_I3_GuillaumeCollin_Correction/UAA14_I3_GuillaumeCollin/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UAA14_I3_GuillaumeCollin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MethodesDuProjet methode = new MethodesDuProjet();
+        public MainWindow()
+        {
+            InitializeComponent();
+            txt1.PreviewTextInput += new TextCompositionEventHandler(verifdonnée);
+            txt2.PreviewTextInput += new TextCompositionEventHandler(verifdonnée);
+            noutcalc.Click += new RoutedEventHandler(calcule);
+            noutres.Click += new RoutedEventHandler(reset);
+
+
+        }
+        private void verifdonnée(object senders, TextCompositionEventArgs e)
+        {
+            if (e.Text =="1" || e.Text=="0" )
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+        private void calcule(object senders, RoutedEventArgs e)
+        {
+
+            if (txt1.Text != "" && txt2.Text != "" && txt1.Text.Length < 8 && txt2.Text.Length < 8)
+            {
+                ushort[] TBN1;
+                ushort[] TBN2;
+                MethodesDuProjet mesoutils = new MethodesDuProjet();
+                if (Convert.ToInt32(txt2.Text.ToString()) < Convert.ToInt32(txt1.Text.ToString()))
+                {
+                    TBN1 = new ushort[txt1.Text.Length];
+                    TBN1 = mesoutils.RemplirTableau(txt1.Text);
+                    TBN2 = new ushort[txt2.Text.Length];
+                    TBN2 = mesoutils.RemplirTableau(txt2.Text);
+                }
+                else
+                {
+
+                    TBN1 = new ushort[txt2.Text.Length];
+                    TBN1 = mesoutils.RemplirTableau(txt1.Text);
+                    TBN2 = new ushort[txt1.Text.Length];
+                    TBN2 = mesoutils.RemplirTableau(txt2.Text);
+                }
+                if (add.IsChecked == true)
+                {
+                    ushort[] tRes;
+                    bool ok;
+                    mesoutils.Additionne(TBN1 , TBN2,out tRes,out ok);
+                    if (ok) 
+                    {
+                        texte.Text=mesoutils.Concatene(tRes);
+                    }
+                    else
+                    {
+                        texte.Text = "Dépassement de capacité !";
+                    }
+                }
+               else if (sous.IsChecked == true)
+                {
+                    ushort[] tRes;
+                    if (mesoutils.Soustrait(TBN1, TBN2, out tRes))
+                    {
+                        texte.Text = mesoutils.Concatene(tRes);
+                    }
+                    else
+                    {
+                        texte.Text = "Dépassement de capacité !";
+                    }
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Veuillez encoder deux nombres binaires d'au plus 7 bits.");
+            }
+        }
+        private void reset(object sender, RoutedEventArgs e)
+        {
+            txt1.Text = "";
+            txt2.Text = "";
+            texte.Text = "";
+
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            agencement();
+        }
+        public void  agencement()
+        {
+            ///ColumnDefinition col1 = new ColumnDefinition();
+            ///grille.ColumnDefinitions.Add(col1); 
+            ///Button btnB = new Button();
+            ///btnB.Content = "Calculer";
+            ///btnB.Height = 150;
+            ///btnB.Width = 150;
+            ///btnB.HorizontalAlignment = 0;
+            ///btnB.HorizontalAlignment = HorizontalAlignment.Left;
+            ///Grid.SetColumn(btnB, 0);
+            ///grille.Children.Add(btnB);
+            
+            //definition de la grille
+            ColumnDefinition[] colDef = new ColumnDefinition[3];
+            for (int i = 0; i < 3; i++)
+            {
+                colDef[i] = new ColumnDefinition();
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                grille.ColumnDefinitions.Add(colDef[i]);
+            }
+            RowDefinition[] rowDef = new RowDefinition[3];
+            for (int i = 0; i < 3; i++)
+            {
+                rowDef[i] = new RowDefinition();
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                grille.RowDefinitions.Add(rowDef[i]);
+            }
+
+            ///definition du stack pannel
+            StackPanel stack = new StackPanel();
+
+            Grid.SetColumnSpan(stack, 2);
+
+            //defenition des texte block
+            TextBlock text1= new TextBlock();
+           
+            text1.Text = "confidential liar";
+            text1.FontFamily = new FontFamily("comic sans MS");
+            text1.FontSize = 25;
+            text1.Foreground = Brushes.Red;
+            text1.Background = Brushes.Yellow;
+
+            TextBlock text2 = new TextBlock();
+            text2.Text = "attack:";
+            text1.FontFamily = new FontFamily("comic sans MS");
+            text1.FontSize = 20;
+            text1.Foreground = Brushes.Black;
+            text1.Background = Brushes.Yellow;
+
+
+            stack.Children.Add(text2);
+            Grid.SetColumnSpan(text1, 3);
+            Grid.SetRow(text1, 0);
+
+            grille.Children.Add(text1);
+
+            //definition des boutons
+            Button nouton1 = new Button();
+            Button nouton2 = new Button();
+            Button nouton3 = new Button();
+
+            nouton1.Content = "bouton1";
+            nouton2.Content = "bouton2";
+            nouton3.Content = "bouton3";
+
+            Grid.SetColumn(nouton1,0);
+            Grid.SetRow(nouton1, 1);
+            Grid.SetColumn(nouton2, 1);
+            Grid.SetRow(nouton2, 1);
+            Grid.SetColumn(nouton3, 2);
+            Grid.SetRow(nouton3, 1);
+
+            grille.Children.Add(nouton1);
+            grille.Children.Add(nouton2);
+            grille.Children.Add(nouton3);
+
+          
+
+           //definition de la zone de texte
+           TextBox textBox1 = new TextBox();
+
+            stack.Children.Add(textBox1);
+        }
+
+
+    }
+}
